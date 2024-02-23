62cce5511955d1c62e5a8a40fc7d5d3ef4233881
Anurag Das <ragonscreen@gmail.com>
2024-02-23 12:20:05 +0530

    port from environment variable

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const Datastore = require('nedb');
 require('dotenv').config();
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
